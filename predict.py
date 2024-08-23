ce09221b89f9014a974f43b0f740ade615c2b41b
Devanik Debnath <162272415+Devanik21@users.noreply.github.com>
2024-08-23 14:14:46 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -10,28 +10,28 @@ model = joblib.load("RF_crop recommendation.pkl")  # Replace with your actual mo
 
 # Define colors for each crop
 crop_colors = {
-    "rice": "rgba(240, 255, 240, 0.2)",         # Honeydew
-    "maize": "rgba(255, 228, 181, 0.2)",        # Moccasin
-    "chickpea": "rgba(255, 239, 213, 0.2)",     # Papaya Whip
-    "kidneybeans": "rgba(255, 182, 193, 0.2)",  # Light Pink
-    "pigeonpeas": "rgba(255, 248, 220, 0.2)",   # Cornsilk
-    "mothbeans": "rgba(230, 230, 250, 0.2)",    # Lavender
-    "mungbean": "rgba(152, 251, 152, 0.2)",     # Pale Green
-    "blackgram": "rgba(176, 224, 230, 0.2)",    # Powder Blue
-    "lentil": "rgba(255, 228, 196, 0.2)",       # Bisque
-    "pomegranate": "rgba(255, 160, 122, 0.2)",  # Light Salmon
-    "banana": "rgba(255, 250, 205, 0.2)",       # Lemon Chiffon
-    "mango": "rgba(255, 218, 185, 0.2)",        # Peach Puff
-    "grapes": "rgba(216, 191, 216, 0.2)",       # Thistle
-    "watermelon": "rgba(240, 128, 128, 0.2)",   # Light Coral
-    "muskmelon": "rgba(255, 222, 173, 0.2)",    # Navajo White
-    "apple": "rgba(250, 128, 114, 0.2)",        # Salmon
-    "orange": "rgba(255, 165, 0, 0.2)",         # Orange
-    "papaya": "rgba(144, 238, 144, 0.2)",       # Light Green
-    "coconut": "rgba(245, 222, 179, 0.2)",      # Wheat
-    "cotton": "rgba(255, 228, 225, 0.2)",       # Misty Rose
-    "jute": "rgba(210, 180, 140, 0.2)",         # Tan
-    "coffee": "rgba(139, 69, 19, 0.2)",         # Saddle Brown
+    "rice": "rgba(240, 255, 240, 0.3)",         # Honeydew
+    "maize": "rgba(255, 228, 181, 0.3)",        # Moccasin
+    "chickpea": "rgba(255, 239, 213, 0.3)",     # Papaya Whip
+    "kidneybeans": "rgba(255, 182, 193, 0.3)",  # Light Pink
+    "pigeonpeas": "rgba(255, 248, 220, 0.3)",   # Cornsilk
+    "mothbeans": "rgba(230, 230, 250, 0.3)",    # Lavender
+    "mungbean": "rgba(152, 251, 152, 0.3)",     # Pale Green
+    "blackgram": "rgba(176, 224, 230, 0.3)",    # Powder Blue
+    "lentil": "rgba(255, 228, 196, 0.3)",       # Bisque
+    "pomegranate": "rgba(255, 160, 122, 0.3)",  # Light Salmon
+    "banana": "rgba(255, 250, 205, 0.3)",       # Lemon Chiffon
+    "mango": "rgba(255, 218, 185, 0.3)",        # Peach Puff
+    "grapes": "rgba(216, 191, 216, 0.3)",       # Thistle
+    "watermelon": "rgba(240, 128, 128, 0.3)",   # Light Coral
+    "muskmelon": "rgba(255, 222, 173, 0.3)",    # Navajo White
+    "apple": "rgba(250, 128, 114, 0.3)",        # Salmon
+    "orange": "rgba(255, 165, 0, 0.3)",         # Orange
+    "papaya": "rgba(144, 238, 144, 0.3)",       # Light Green
+    "coconut": "rgba(245, 222, 179, 0.3)",      # Wheat
+    "cotton": "rgba(255, 228, 225, 0.3)",       # Misty Rose
+    "jute": "rgba(210, 180, 140, 0.3)",         # Tan
+    "coffee": "rgba(139, 69, 19, 0.3)",         # Saddle Brown
 }
 
 
